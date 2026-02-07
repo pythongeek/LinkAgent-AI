9e14c27c76ce040ab0d41447c362221013a5ebc1
pythongeek <tsnion@gmail.com>
2026-02-07 18:03:29 +0600

    Remove 401 redirect to /login

--- src/services/api.ts
+++ src/services/api.ts
@@ -22,10 +22,6 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem('token');
-      window.location.href = '/login';
-    }
     return Promise.reject(error);
   }
 );
